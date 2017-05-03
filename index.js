051a8dc05d4b1309ba1c37be79e280fb6ad15971
nickiitsu <pannatorn.po@skru.ac.th>
2017-05-03 10:55:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-app.post('/webhook', function(req, res) {
+app.get('/webhook', function(req, res) {
     res.sendStatus(200)
 })
