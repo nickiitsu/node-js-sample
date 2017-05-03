7fddb6d9c8ddecc2fae303cfb51602e7c2a715dd
nickiitsu <pannatorn.po@skru.ac.th>
2017-05-03 13:41:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var app = express()
 
 app.use(bodyParser.json())
 
-app.set('port', (process.env.PORT || 4000))
+app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
