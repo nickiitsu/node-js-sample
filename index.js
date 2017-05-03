2cc8214bd434c720035b592b849cd857b7bc2f97
nickiitsu <pannatorn.po@skru.ac.th>
2017-05-03 13:46:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!www')
 })
 
-app.listen(app.get('port'), function() {
+/*app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-
+*/
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
 })
